c4520d5dc2ed44c633b80c499e9a150013158626
QyuGH <markanton.cahutay@students.isatu.edu.ph>
2024-08-13 20:24:58 +0800

    final diamond

--- Diamond.java
+++ Diamond.java
@@ -15,8 +15,14 @@ public class Diamond{
             System.out.println();
         }
 
-        for (int i = 1; i < num; i++){
-            
+        for (int i = num - 1; i >= 1; i--){
+            for (int j = 1; j <= num - i; j++){
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i * 2 - 1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
 
     }
